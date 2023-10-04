5869c90fff8121360367d4e4a003f7258789fe40
bighk <wnsvmfh77@gmail.com>
2023-10-04 23:36:42 +0900

    feat:category페이지 개발서버로 연결.

--- app.js
+++ app.js
@@ -8,31 +8,68 @@ const pingRouter = require("./router/PingRouter");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('public'));
 
-// dotenv
 dotenv.config();
 
+// // mongoose
 // mongoose
-mongoose
-  .connect(process.env.MONGO_URI, {
-    dbName: process.env.MONGO_DB_NAME,
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log(err));
+//   .connect(process.env.MONGO_URI, {
+//     dbName: process.env.MONGO_DB_NAME,
+//   })
+//   .then(() => console.log("MongoDB Connected..."))
+//   .catch((err) => console.log(err));
+
+const categoryPath = __dirname + '/views/pages/Categorypage';
+
 
 // router
-app.use("/api/ping", pingRouter);
-
-// error handling
-app.use((err, req, res) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Something went wrong.";
-  res.status(statusCode);
-  res.json({ message });
+app.get('/category', (req, res) => {
+  res.sendFile(categoryPath + '/category.html')
 });
 
+
+
+
+app.get('/api/category/best', (req, res) => {
+
+  res.send([{
+    name: '네이비 에코백',
+    price: 100000,
+    src: '/static/images/navybag.jpeg',
+    link: 'https://jjongtaeng.github.io/linkevery'
+  },
+  {
+    name: '체크무늬 에코백',
+    price: 120000,
+    src: '/static/images/checkback.jpeg',
+    link: 'https://jjongtaeng.github.io/linkevery'
+  },{
+    name: '초록 에코백',
+    price: 87500,
+    src: '/static/images/greenbag.jpeg',
+    link: 'https://jjongtaeng.github.io/linkevery'
+  },{
+    name: '파랑 에코백',
+    price: 19000,
+    src: '/static/images/bluebag.jpeg',
+    link: 'https://jjongtaeng.github.io/linkevery'
+  }])
+})
+
+// app.use("/api/ping", pingRouter);
+
+// // error handling
+// app.use((err, req, res) => {
+//   const statusCode = err.statusCode || 500;
+//   const message = err.message || "Something went wrong.";
+//   res.status(statusCode);
+//   res.json({ message });
+// });
+
 // port
-const port = process.env.PORT;
+// const port = process.env.PORT;
+const port = 5050;
 
 app.listen(port, () => {
   console.log(`Connecting to PORT ${port}`);
---
+++ views/pages/Categorypage/category.js
@@ -0,0 +1,16 @@
+
+let data = axios.get('/api/category/best').then(console.log);
+
+
+
+
+
+    // METHOD GET
+    // path /api/best
+
+    // response
+    //     [{
+    //         name
+    //         species
+    //         price
+    //     }]
