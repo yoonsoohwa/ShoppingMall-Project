946b98b8a19c2d14b3f2b465c5df52f82a1fff3a
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-10-06 03:47:44 +0900

    feat:adminpage-product

---
+++ views/js/Admin/product/add.js
@@ -0,0 +1,182 @@
+const categoryInput = document.querySelector('#category-input')
+const nameInput = document.querySelector('#name-input')
+const priceInput = document.querySelector('#price-input')
+const imageInput = document.querySelector('#img-input')
+const detailImageInput = document.querySelector('#detail-img-input')
+const colorInput = document.querySelector('#color-input')
+const sizeInput = document.querySelector('#size-input')
+const contentInput = document.querySelector('#content-input')
+
+const imagePreview = document.getElementById('image-preview');
+const detailImagePreview = document.getElementById('detail-image-preview');
+const delImgBtn = document.querySelector('.delete-img')
+const delDetailImgBtn = document.querySelector('.delete-detimg')
+
+
+
+/* 이미지 미리보기, 용량제한, 수정, 삭제 */
+
+// 대표 이미지
+imageInput.addEventListener('change', function (e) {
+  const file = e.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const img = document.createElement('img');
+      img.src = e.target.result;
+      img.style.maxWidth = '150px';
+      imagePreview.innerHTML = ''; // 이미지를 미리보기 전에 이전 이미지를 지웁니다.
+      imagePreview.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+    delImgBtn.style.display = "block";
+  } 
+});
+
+// 대표 이미지 삭제
+delImgBtn.addEventListener('click', () => {
+  imagePreview.innerHTML = '';
+  imageInput.value = '';
+  delImgBtn.style.display = "none";
+})
+
+
+
+// 상세 이미지
+let selectedImages = []; // ex) ["a.png", "b.png", "c.jpg", ...]
+const maxImageSize = 5 * 1024 * 1024  // 최대 용량 5MB
+
+
+detailImageInput.addEventListener('change', function () {
+  const files = detailImageInput.files;
+  addImagesToSelected(files);
+});
+
+function addImagesToSelected(files) {
+  let totalSize = 0
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (file.type.startsWith('image/')) {
+      const fileSize = file.size
+      // 용량 제한
+      if (totalSize + fileSize <= maxImageSize) {
+        selectedImages.push(file.name);
+        totalSize += fileSize;
+
+        const parentDiv = document.createElement('div'); // img, checkbox의 부모div
+        parentDiv.style.display = 'inline-block'
+
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+          const img = document.createElement('img');
+          img.src = e.target.result;
+          img.style.maxWidth = '150px';
+
+          // 체크박스 생성
+          const deleteCheckbox = document.createElement('input');
+          deleteCheckbox.type = 'checkbox';
+          deleteCheckbox.className = 'position-absolute';
+          deleteCheckbox.id = 'delete-check-btn';
+          parentDiv.appendChild(img);
+          parentDiv.appendChild(deleteCheckbox);
+          detailImagePreview.appendChild(parentDiv); // 미리보기에 img,checkbox 담기
+          delDetailImgBtn.style.display = "block";
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert('이미지 용량이 최대 용량을 초과했습니다.');
+        detailImageInput.value = '';
+      }
+    }
+  }
+}
+
+// 부분 이미지 파일 삭제
+delDetailImgBtn.addEventListener('click', () => {
+  const checkedItems = document.querySelectorAll('#delete-check-btn');
+  let delImages = []; // 삭제된 이미지들
+  
+  checkedItems.forEach((checkbox) => {
+    if (checkbox.checked) {
+      const parentDiv = checkbox.parentNode;
+      parentDiv.remove();
+
+      // 체크된 이미지의 이름 추출
+      const imgName = parentDiv.querySelector('img').src.split('/').pop();
+      delImages.push(imgName)
+    }
+  });
+
+  // selectedImages 배열에서 삭제
+  selectedImages = selectedImages.filter((image) => !delImages.includes(image));
+
+  // selectedImages가 없으면, 삭제버튼 안보이도록
+  if (!selectedImages) {
+    delDetailImgBtn.style.display = 'none'; 
+  }
+});
+
+
+
+
+/* db에 생성한 정보 전달 (post)*/
+const submitBtn = document.querySelector('.submit')
+submitBtn.addEventListener('click', handleSubmit)
+
+async function handleSubmit(e) {
+  e.preventDefault()
+  
+  // 입력값 가져오기
+  const category = categoryInput.value
+  const image = imageInput.value
+  const detail_image = selectedImages
+  const name = nameInput.value
+  const price = priceInput.value
+  const color = colorInput.value.replace(/\s/g, '').split(',')
+  const size = sizeInput.value.replace(/\s/g, '').split(',')
+  const content = contentInput.value
+  
+  // 필수 입력 필드 검사
+  if (!category || !name || !price || !image) {
+    alert('카테고리, 이름, 가격, 대표 이미지는 필수 입력 필드입니다. 모든 필수 입력 필드를 작성하세요.');
+    return; 
+  }
+
+  const data = {
+    category: category,
+    image: image,
+    detail_image: detail_image,
+    name: name,
+    price: Number(price),
+    option: {
+      color: color,
+      size: size,
+    },
+    content: content,
+  }
+  
+  const dataJson = JSON.stringify(data)
+  
+  const apiUrl = `` // url 추가
+
+  try {
+    const res = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: dataJson,
+    }); 
+    if (res.ok) {
+      alert('상품 등록이 완료되었습니다!')
+      // 상품목록 페이지(admin.html)로 이동 코드 추가
+    } else {
+      console.error('생성 요청 실패:', res.status);
+    }
+  } catch (error) {
+    console.error('생성 요청 중 오류 발생:', error);
+  }
+}
---
+++ views/js/Admin/product/modify.js
@@ -0,0 +1,206 @@
+// 현재 URL에서 쿼리스트링 가져오기
+const queryString = window.location.search;
+const queryId = queryString.split('?')[1]
+
+const id = document.getElementById('pro-id');
+id.innerHTML = queryId
+const mCategory = document.getElementById('m-category-input');
+const mImage = document.getElementById('m-img-input');
+const mDetailImage = document.getElementById('m-detail-img-input'); //>>수정
+const mName = document.getElementById('m-name-input');
+const mPrice = document.getElementById('m-price-input');
+const mColor = document.getElementById('m-color-input');
+const mSize = document.getElementById('m-size-input');
+const mContent = document.getElementById('m-content-input');
+
+const imagePreview = document.getElementById('image-preview');
+const detailImagePreview = document.getElementById('detail-image-preview');
+const delDetailImgBtn = document.querySelector('.delete-detimg')
+
+
+// db에서 id값과 일치하는 데이터 불러오기 (get)
+async function getProductData(id) {
+  try {
+    const res = await fetch(``);
+    const data = await res.json();
+
+    // 상품목록에서 받아온 데이터를 default 값으로 두고 수정
+    mCategory.value = data.category;
+    imagePreview.innerHTML = `<img src="${data.image}" style="max-width: 150px">`;
+    data.detail_image.forEach((val) => {
+      detailImagePreview += `<img src="${val}" style="max-width: 150px">`
+    })
+    mName.placeholder = data.name;
+    mPrice.placeholder = data.price;
+    mColor.placeholder = data.option.color.join(', ')
+    mSize.placeholder = data.option.color.join(', ')
+    mContent.placeholder = data.content;
+  } catch (error) {
+    console.error('데이터를 가져오는 중 에러 발생:', error);
+  }
+}
+
+getProductData(queryId);
+
+
+
+
+/* 이미지 미리보기, 용량제한, 수정, 삭제 */
+
+// 대표 이미지
+mImage.addEventListener('change', function (e) {
+  const file = e.target.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const img = document.createElement('img');
+      img.src = e.target.result;
+      img.style.maxWidth = '150px';
+      imagePreview.innerHTML = ''; // 초기화
+      imagePreview.appendChild(img);
+    };
+    reader.readAsDataURL(file);
+  } else {
+    imagePreview.innerHTML = ''; 
+  }
+});
+
+
+// 상세 이미지
+let selectedImages = []; // ex) ["a.png", "b.png", "c.jpg", ...]
+const maxImageSize = 5 * 1024 * 1024  // 최대 용량 5MB
+
+
+mDetailImage.addEventListener('change', function () {
+  const files = mDetailImage.files;
+  addImagesToSelected(files);
+});
+
+function addImagesToSelected(files) {
+  let totalSize = 0
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (file.type.startsWith('image/')) {
+      const fileSize = file.size
+      // 용량 제한
+      if (totalSize + fileSize <= maxImageSize) {
+        selectedImages.push(file.name);
+        totalSize += fileSize;
+
+        const parentDiv = document.createElement('div'); // img, checkbox의 부모div
+        parentDiv.style.display = 'inline-block'
+
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+          const img = document.createElement('img');
+          img.src = e.target.result;
+          img.style.maxWidth = '150px';
+
+          // 체크박스 생성
+          const deleteCheckbox = document.createElement('input');
+          deleteCheckbox.type = 'checkbox';
+          deleteCheckbox.className = 'position-absolute';
+          deleteCheckbox.id = 'delete-check-btn';
+          parentDiv.appendChild(img);
+          parentDiv.appendChild(deleteCheckbox);
+          detailImagePreview.appendChild(parentDiv); // 미리보기에 img,checkbox 담기
+          delDetailImgBtn.style.display = "block";
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert('이미지 용량이 최대 용량을 초과했습니다.');
+        mDetailImage.value = '';
+      }
+    }
+  }
+}
+
+// 부분 이미지 파일 삭제
+delDetailImgBtn.addEventListener('click', () => {
+  const checkedItems = document.querySelectorAll('#delete-check-btn');
+  let delImages = []; // 삭제된 이미지들
+  
+  checkedItems.forEach((checkbox) => {
+    if (checkbox.checked) {
+      const parentDiv = checkbox.parentNode;
+      parentDiv.remove();
+
+      // 체크된 이미지의 이름 추출
+      const imgName = parentDiv.querySelector('img').src.split('/').pop();
+      delImages.push(imgName)
+    }
+  });
+
+  // selectedImages 배열에서 삭제
+  selectedImages = selectedImages.filter((image) => !delImages.includes(image));
+
+  // selectedImages가 없으면, 삭제버튼 안보이도록
+  if (!selectedImages) {
+    delDetailImgBtn.style.display = 'none'; 
+  }
+});
+
+
+
+
+/* db에 수정한 정보 전달 (put) */
+const submitBtn = document.querySelector('.submit')
+submitBtn.addEventListener('click', handleSubmit)
+
+async function handleSubmit(e) {
+  e.preventDefault()
+  
+  // 입력값 가져오기
+  const category = mCategory.value
+  const image = mImage.value
+  const detail_image = selectedImages
+  const name = mName.value
+  const price = mPrice.value
+  const color = mColor.value.replace(/\s/g, '').split(',')
+  const size = mSize.value.replace(/\s/g, '').split(',')
+  const content = mContent.value
+
+  // 필수 입력 필드 검사
+  if (!category || !name || !price || !image) {
+    alert('카테고리, 이름, 가격, 대표 이미지는 필수 입력 필드입니다. 모든 필수 입력 필드를 작성하세요.');
+    return; 
+  }
+
+  const data = {
+    category: category,
+    image: image,
+    detail_image: detail_image,
+    name: name,
+    price: Number(price),
+    option: {
+      color: color,
+      size: size,
+    },
+    content: content,
+  }
+  
+  const dataJson = JSON.stringify(data)
+  
+  const apiUrl = ``  // url 추가
+  try {
+    const res = await fetch(apiUrl, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: dataJson,
+    });  
+    if (res.ok) {
+      alert('상품 수정이 완료되었습니다!')
+      // 상품목록 페이지(admin.html)로 이동 코드 추가
+    } else {
+      console.error('수정 요청 실패:', res.status);
+    }
+  } catch (error) {
+    console.error('수정 요청 중 오류 발생:', error);
+  }
+  
+}
