08b74fdb4e933de050dcdbff6cfd8073d3b07444
Jiwon <hajw.study@gmail.com>
2023-10-06 18:16:28 +0900

    장바구니 1차 구현

---
+++ views/js/basket/products.js
@@ -0,0 +1,145 @@
+export const products = [{
+  name: '상품명1',
+  price: 30000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명2',
+  price: 15000,
+  count: 0,
+  deliveryPrice: 0,
+  images: './image/이미지이름.png',
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명3',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명4',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명5',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명6',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명7',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명8',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+},
+{
+  name: '상품명9',
+  price: 20000,
+  count: 0,
+  deliveryPrice: 0,
+  option: [
+  {
+    color: 'red',
+    size: 'S',
+  },
+  {
+    color: 'blue',
+    size: 'L',
+  },
+  ]
+}];
