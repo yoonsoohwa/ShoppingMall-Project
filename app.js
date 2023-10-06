41345f8cf764664179cf282cf0154f750aa5e110
donghyun123 <numnum97@naver.com>
2023-10-06 23:05:48 +0900

    dotenv 복구

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // dotenv
-dotenv.config();
+console.log(
+  `어플리케이션 서버를 다음 환경으로 시작합니다: ${process.env.NODE_ENV}`
+);
+if (process.env.NODE_ENV === "dev") {
+  dotenv.config({ path: ".env" });
+}
+if (process.env.NODE_ENV === "prod") {
+  dotenv.config({ path: ".env.prod" });
+}
 
 // mongoose
 mongoose
@@ -24,7 +32,6 @@ mongoose
   .catch((err) => console.log(err));
 
 // router
-
 app.use("/api/v1/ping", pingRouter);
 app.use("/api/v1/orders", orderRouter);
 app.use("/api/v1/items", itemsRouter);
