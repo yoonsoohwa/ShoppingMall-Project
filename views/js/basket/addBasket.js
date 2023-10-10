51c6b0c51b9600337fe199f192dd030285fffeba
Jiwon <hajw.study@gmail.com>
2023-10-10 19:34:46 +0900

    장바구니 기능 2차 구현 및 주문 정보 퍼블리싱

--- views/js/basket/addBasket.js
+++ views/js/basket/addBasket.js
@@ -1,20 +1,22 @@
 // import { eventListener } from "./display.js";
-import { drawBasket } from "./index.js";
-import { products } from "./products.js";
+import { drawBasket, baskets } from './index.js';
+import { products } from './products.js';
 
 // 장바구니 상품 추가 함수에 click이벤트를 달아주는 함수
-const addBasketButton = document.querySelector("#addProduct");
-addBasketButton.addEventListener("click", () => {
-  addBasket("상품명2", 1, {
-    color: "red",
-    size: "S",
+const addBasketButton = document.querySelector('#addProduct');
+addBasketButton.addEventListener('click', () => {
+  addBasket('상품명2', 1, {
+    color: 'red',
+    size: 'S',
   });
 });
 
 // 로컬 스토리지에 있는 basket 장바구니를 리턴 해주는 함수
 export const getBasket = () => {
-  const localStorageBasket = localStorage.getItem("basket");
+  const localStorageBasket = localStorage.getItem('basket');
   const parseBasket = JSON.parse(localStorageBasket);
+
+  isEmptyBasket(parseBasket);
 
   return parseBasket;
 };
@@ -27,36 +29,39 @@ export const addBasket = (name, count, option) => {
     ...selectedProduct,
     count,
     option,
+    checked: false,
   };
+
+  baskets.push(parseProduct);
   const basket = getBasket();
   basket.push(parseProduct);
 
-  localStorage.setItem("basket", JSON.stringify(basket));
+  localStorage.setItem('basket', JSON.stringify(basket));
 
   drawBasket();
-  eventListener();
-};
-
-// 장바구니에 선택 한 상품을 삭제 하는 함수
-export const removeBasket = (name) => {
-  const basket = getBasket();
-  const withoutSelectedProduct = basket.filter(
-    (product) => product.name !== name
-  );
-
-  localStorage.setItem("basket", JSON.stringify(withoutSelectedProduct));
+  // eventListener();
 };
 
 // 만약 장바구니가 비어있으면 장바구니가 비어있는 화면을, 아니라면 채워져있는 화면을 보여준다.
-export const isEmptyBasket = () => {
-  const hasElement = document.querySelector(".has");
-  const emptyElement = document.querySelector(".empty");
+export const isEmptyBasket = (parseBasket) => {
+  const hasElement = document.querySelector('.has');
+  const emptyElement = document.querySelector('.empty');
 
-  if (parseBasket.length == 0) {
-    hasElement.style.display = "none";
-    emptyElement.style.display = "block";
+  if (parseBasket.length === 0) {
+    hasElement.style.display = 'none';
+    emptyElement.style.display = 'block';
   } else {
-    hasElement.style.display = "block";
-    emptyElement.style.display = "none";
+    hasElement.style.display = 'block';
+    emptyElement.style.display = 'none';
   }
 };
+
+// 체크 한 함수 넣어주는 함수
+
+// checkElements.forEach((checkedElement, index) => {
+//   const baskets = getBasket();
+
+//   checkedElement.addEventListener('click', clickCheck);
+
+//   // const checekdBaskets = baskets.map((product) => {});
+// });
