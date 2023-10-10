2cd30f3263bc18583c6730db0383141d0cdadaa3
donghyun123 <numnum97@naver.com>
2023-10-10 12:14:35 +0900

    Feat: dummy user 추가

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const orderRouter = require('./routes/OrderRouter');
 const itemsRouter = require('./routes/ItemsRouter');
 const userRouter = require('./routes/UserRouter');
 
+const { User } = require('./models/User');
+
+const dummy = require('./dummy.json');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -28,6 +32,12 @@ mongoose
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
+
+// Add dummy data
+dummy.users.forEach(async (user) => {
+  const newUser = new User(user);
+  await newUser.save();
+});
 
 // router
 app.use('/api/v1/ping', pingRouter);
