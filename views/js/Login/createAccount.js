78556b613ef9821c349290cc7b3555ad295bf43d
bighk <wnsvmfh77@gmail.com>
2023-10-10 20:09:45 +0900

    feat: 회원가입 기능 구현

--- views/js/Login/createAccount.js
+++ views/js/Login/createAccount.js
@@ -1,44 +1,48 @@
-// import { response } from "express";
-// import { User } from "../../../models/Category.js";
-// import { register } from "../../../services/userService.js";
+import { response } from "express";
+import { User } from "../../../models/Category.js";
+import { register } from "../../../services/userService.js";
 
 
-// document.getElementById('register-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-    const dataList = document.querySelector('#register-form');
-    const dataSubmit = document.querySelector('#data-submit');
 
+    const id = document.querySelector('.id');
+    const userName = document.querySelector('.user-name');
+    const password = document.querySelector('.password');
+    const role = document.querySelector('.role');
+    const address = document.querySelector('.address');
+    const phoneNumber = document.querySelector('.phone-number');
+    const email = document.querySelector('.email');
+    const dataSubmit = document.querySelector('#data-submit')
+
+
+    
     dataSubmit.addEventListener('click', (e) => {
         e.preventDefault();
-    
-    // 입력된 데이터 가져오기
-    let id = document.getElementById('id').value;
-    let name = document.getElementById('name').value;
-    let password = document.getElementById('password').value;
-    let role = document.getElementById('role').value;
-    let address = document.getElementById('address').value;
-    let phoneNumber = document.getElementById('phone-number').value;
-    let email = document.getElementById('email').value;
+        
+        let data = {
+            id: id.value,
+            userName: userName.value,
+            password: password.value,
+            role: role.value,
+            address: address.value,
+            phoneNumber: phoneNumber.value,
+            email: email.value,
+        };
 
+        const jsonData = JSON.stringify(data);  // JSON 문자열로 변환.
+        
+        fetch('', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: jsonData, // 변환한 JSON 문자열을 요청의 본문에 포함시킴
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch((error) => console.error('Error:', error));
 
-    // 서버에 전송할 데이터 객체 생성
-    let data = {
-        id: id,
-        name: name,
-        password: password,
-        role: role,
-        address: address,
-        phoneNumber: phoneNumber,
-        email: email,
-    };
+        console.log(data)
+    })
 
-    // fetch('/register', { 
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify(data),
-    // })
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch((error) => console.error('Error:', error));
-    console.log(data)
-    });
+    //try catch
+    //async await
