d4ce1c423eb92e2b0a75c415bbdbe3f7b6f3c54b
donghyun123 <numnum97@naver.com>
2023-10-11 17:49:51 +0900

    feat: subdir 까지

--- routes/viewsRouter.js
+++ routes/viewsRouter.js
@@ -4,6 +4,14 @@ const path = require('path');
 const viewsRouter = express.Router();
 
 viewsRouter.use('/', express.static('views'));
+viewsRouter.use('/:directory/:subdirectory/:file', (req, res, next) => {
+  const { directory, subdirectory, file } = req.params;
+  if (!file.includes('.')) {
+    res.sendFile(path.join(__dirname, `../views/${directory}/${subdirectory}/${file}.html`));
+  } else {
+    next();
+  }
+});
 viewsRouter.use('/:directory/:file', (req, res, next) => {
   const { directory, file } = req.params;
   if (!file.includes('.')) {
