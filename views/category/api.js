6f77e3b7d0a9a21b326a877ff426de5e2dd07922
현규 박 <wnsvmfh77@gmail.com>
2023-10-12 20:01:21 +0900

    feat: 카테고리 api 연결

--- views/category/api.js
+++ views/category/api.js
@@ -1,46 +1,16 @@
-export const getProductList = () => {
-    return [
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
-        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
-        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
-        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
-        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
-        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
-        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
-        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
-        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
-        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
-        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
-        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
-        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},    
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
-        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
-        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
-        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
-        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
-        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
-        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},    
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
-        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
-        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
-        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
-        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
-        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
-        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
-        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
-        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},
-    ]
+export const getProductList = async() => {
+    try {
+        const res = await fetch('/api/v1/items', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await res.json();
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const getBestProductList = () => {
@@ -70,24 +40,9 @@ export const getBestProductList = () => {
     ]
 }
 
-export const getCategoryDataList = async() => {
-    try {
-        const res = await fetch('/api/v1/categories', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(),
-        })
-        const data = await res.json();
-        console.log(data);
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-/*
-{
+export const getCategoryDataList = () => {
+    return [
+        {
             "_id": "652537044a9f5d7ac941331f",
             "name": "TOP",
             "__v": 0
@@ -132,4 +87,51 @@ export const getCategoryDataList = async() => {
             "name": "ETC",
             "__v": 0
         }
+    ]
+}
+
+
+/*
+return [
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
+        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
+        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
+        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
+        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
+        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
+        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
+        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
+        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
+        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
+        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
+        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
+        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},    
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
+        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
+        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
+        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
+        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
+        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
+        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},    
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537044a9f5d7ac941331f"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537044a9f5d7ac941331f"},
+        {id : 2, price : 20000, showedPrice : '20,000₩', title : 'navybag3', img : './imgtest/imgtest3.jpeg', date: '2023-09-02 12:21', category: "652537144a9f5d7ac9413322"},
+        {id : 3, price : 40000, showedPrice : '40,000₩', title : 'navybag4', img : './imgtest/imgtest4.jpeg', date: '2023-09-01 12:21', category: "652537484a9f5d7ac9413327"},
+        {id : 4, price : 90000, showedPrice : '90,000₩', title : 'navybag5', img : './imgtest/imgtest5.jpeg', date: '2023-09-09 17:21', category: "6525374e4a9f5d7ac9413329"},
+        {id : 5, price : 60000, showedPrice : '60,000₩', title : 'navybag6', img : './imgtest/imgtest6.jpeg', date: '2023-09-09 11:21', category: "6525375e4a9f5d7ac941332d"},
+        {id : 6, price : 45000, showedPrice : '45,000₩', title : 'navybag7', img : './imgtest/imgtest7.jpeg', date: '2023-09-12 10:21', category: "652537644a9f5d7ac941332f"},
+        {id : 7, price : 80000, showedPrice : '80,000₩', title : 'navybag8', img : './imgtest/imgtest8.jpeg', date: '2023-09-08 12:21', category: "652537554a9f5d7ac941332b"},
+        {id : 0, price : 30000, showedPrice : '30,000₩', title : 'navybag1', img : './imgtest/imgtest1.jpeg', date: '2023-09-09 13:21', category: "652537144a9f5d7ac9413322"},
+        {id : 1, price : 20000, showedPrice : '20,000₩', title : 'navybag2', img : './imgtest/imgtest2.jpeg', date: '2023-09-09 15:21', category: "652537694a9f5d7ac9413331"},
+    ]
 */
