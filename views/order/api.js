74d1c046cf9bd5117da1c3f0eb51b3f8a876e994
Jiwon <hajw.study@gmail.com>
2023-10-12 11:36:19 +0900

    확인용 commit

--- views/order/api.js
+++ views/order/api.js
@@ -1,5 +1,3 @@
-fetch('http://localhost:5001/api/v1/orders/1/20')
+fetch('http://localhost:5001/api/v1/orders/get/guest')
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+  .then((data) => console.log(data));
