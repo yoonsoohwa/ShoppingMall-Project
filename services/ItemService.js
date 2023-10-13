0615638972b7e618250a0bfe4da0342574273a2b
jeongsujong <jeon95ujong@gmail.com>
2023-10-13 10:00:34 +0900

    d

--- services/ItemService.js
+++ services/ItemService.js
@@ -26,14 +26,6 @@ class ItemService {
     return deletedItem;
   }
 
-  async deleteItems(itemIds) {
-    const deletedItem = await Item.deleteMany({ _id: itemIds });
-    if (!deletedItem) {
-      throw new NotFoundError('해당 아이템을 찾을 수 없습니다.');
-    }
-    return deletedItem;
-  }
-
   async updateItem(itemId, updatedItemData) {
     const updatedItem = await Item.findByIdAndUpdate(itemId, updatedItemData, {
       new: true,
