92ade05ccc168a02bb3ff7447ad5e93b38d8ab6e
[SW6] 이선우 <sunwoo40101@gmail.com>
2023-10-13 19:34:36 +0900

    feat: admin page 상품 삭제 기능 수정

--- views/admin/product.js
+++ views/admin/product.js
@@ -1,11 +1,15 @@
+const total = document.querySelector('.total');
+const tableBody = document.querySelector('#table-body');
+const checkAllBtn = document.querySelector('#check-all');
+
 /* db에서 전체 목록 불러오기(get) */
 async function insertProductData() {
   try {
     const res = await fetch(`/api/v1/items`);
     const data = await res.json();
+    tableBody.innerHTML = '';
 
     data.forEach((product) => {
-      const tableBody = document.querySelector('#table-body');
       const rowHTML = `
         <td><input class="form-check-input" type="checkbox" id="check-item"></td>
         <td id="product-id">${product._id}</td>
@@ -17,40 +21,40 @@ async function insertProductData() {
         <td id="product-modify"><button type="button" class="pro-modify btn btn-secondary btn-sm">수정</button></td>
       `;
       tableBody.insertAdjacentHTML('beforeend', `<tr>${rowHTML}</tr>`);
+
+      // 총 개수 update
+      const allRow = document.querySelectorAll('#table-body tr');
+      total.innerText = allRow.length > 1 ? `[총 ${allRow.length - 1}개]` : `[총 0개]`;
     });
   } catch (error) {
     alert('데이터를 가져오는 중 에러 발생:', error);
   }
 }
-insertProductData();
+// insertProductData();
 
-/* 총 개수 */
-const total = document.querySelector('.total');
-const allRow = document.querySelectorAll('#table-body tr');
-total.innerHTML = allRow.length > 1 ? `[총 ${allRow.length - 1}개]` : `[총 0개]`;
+// ------------------------------------------
 
-/* 상품 삭제 */
-const checkAllBtn = document.querySelector('#check-all');
-const checkboxes = document.querySelectorAll('#check-item');
+// 전체 선택
+function selectAllCheckboxes() {
+  const checkBoxList = Array.from(document.querySelectorAll('#check-item'));
 
-checkAllBtn.addEventListener('change', () => {
   if (checkAllBtn.checked) {
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = true;
+    checkBoxList.forEach((el) => {
+      const element = el;
+      element.checked = true;
     });
   } else {
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = false;
+    checkBoxList.forEach((el) => {
+      const element = el;
+      element.checked = false;
     });
   }
-});
+}
 
-const deleteBtn = document.querySelector('.pro-delete');
-deleteBtn.addEventListener('click', async (e) => {
-  e.preventDefault();
-
-  // 체크된 항목의 index를 저장할 배열
-  const checkedItemsIndex = [];
+/* 상품 삭제 */
+function delItems() {
+  const checkboxes = document.querySelectorAll('#check-item');
+  const checkedItemsIndex = []; // 체크된 항목의 index를 저장할 배열
 
   checkboxes.forEach((checkbox, index) => {
     if (checkbox.checked) {
@@ -58,64 +62,66 @@ deleteBtn.addEventListener('click', async (e) => {
     }
   });
 
-  // 확인 창 표시
-  if (checkedItemsIndex.length > 0) {
-    const confirmMessage = `체크된 항목 ${checkedItemsIndex.length}개를 삭제하시겠습니까?`;
-
-    if (window.confirm(confirmMessage)) {
-      // 확인
-      checkedItemsIndex.forEach((index) => {
-        const row = checkboxes[index].closest('tr'); // 체크된 체크박스의 부모 <tr> 요소 찾기
-        if (row) {
-          row.remove();
-        }
-      });
-      checkAllBtn.checked = false;
-
-      // 삭제된 항목의 id를 저장한 배열 => db로 id list(checkedItemsId) 전달
-      const checkedItemsId = [];
-      checkedItemsIndex.forEach((index) => {
-        const row = checkboxes[index].closest('tr');
-        if (row) {
-          const productId = row.querySelector('#product-id').textContent;
-          checkedItemsId.push(productId);
-        }
-      });
-
-      // db에 삭제한 정보 전달 (delete)
-      const apiUrl = `/api/v1/items`;
-      const reqData = JSON.stringify({ itemIds: checkedItemsId });
-
-      try {
-        const res = await fetch(apiUrl, {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: reqData,
-        });
-
-        const result = await res.json();
-        if (res.status === 200) {
-          alert(result.message);
-          await insertProductData(); // 데이터 다시 불러오기
-        } else {
-          alert(result.message);
-        }
-      } catch (error) {
-        alert('삭제 요청 중 오류 발생:', error);
-      }
-    } else {
-      // 취소
-      checkboxes.forEach((checkbox) => {
-        checkbox.checked = false;
-      });
-      checkAllBtn.checked = false;
-    }
-  } else {
+  if (checkedItemsIndex.length === 0) {
     alert('삭제할 항목을 선택하세요.');
+    return;
   }
-});
+
+  const confirmMessage = `체크된 항목 ${checkedItemsIndex.length}개를 삭제하시겠습니까?`;
+  if (window.confirm(confirmMessage)) {
+    // 확인
+    const checkedItemsId = []; // 삭제된 항목의 id를 저장한 배열
+    checkedItemsIndex.forEach((index) => {
+      const row = checkboxes[index].closest('tr');
+      if (row) {
+        const productId = row.querySelector('#product-id').textContent;
+        checkedItemsId.push(productId);
+      }
+    });
+    postDeleteItem(checkedItemsId);
+    checkAllBtn.checked = false;
+  } else {
+    // 취소
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+    checkAllBtn.checked = false;
+  }
+}
+
+// db에 삭제한 정보 전달 (delete)
+async function postDeleteItem(checkedItemsId) {
+  const apiUrl = `/api/v1/items`;
+  const reqData = JSON.stringify({ itemIds: checkedItemsId });
+
+  try {
+    const res = await fetch(apiUrl, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: reqData,
+    });
+
+    const result = await res.json();
+    if (res.status === 200) {
+      alert(result.message);
+      insertProductData(); // 데이터 다시 불러오기
+    } else {
+      alert(result.message);
+    }
+  } catch (error) {
+    alert('삭제 요청 중 오류 발생:', error);
+  }
+}
+
+insertProductData();
+checkAllBtn.addEventListener('click', selectAllCheckboxes);
+/* 삭제 btn 이벤트 */
+const deleteBtn = document.querySelector('.pro-delete');
+deleteBtn.addEventListener('click', delItems);
+
+// ---------------------------------------------
 
 /* 상품 수정 */
 
