54db1481f004563a786c0a523840cce0dc734976
yoonsoohwa <blue.yoonsh01@gmail.com>
2023-10-14 19:10:28 +0900

    comment: 주석 추가

--- views/admin/order/shipping.js
+++ views/admin/order/shipping.js
@@ -5,6 +5,7 @@ const changeStatusBtn = document.getElementById('change-status');
 
 let orderId;
 
+// 날짜 시간 설정
 function formatDate(createdAt) {
     const orderDate = createdAt.split('.')[0];
     const date = orderDate.split('T')[0];
@@ -13,6 +14,7 @@ function formatDate(createdAt) {
     return `${date} ${time}`;
 }
 
+// 전체 선택 체크박스 설정
 function selectAllCheckboxes() {
     const checkBoxElList = Array.from(document.querySelectorAll('#check-item'));
 
@@ -29,6 +31,7 @@ function selectAllCheckboxes() {
     }
 }
 
+// 주문 목록 생성
 function setOrderList(date, id, addressee, orderItems, totalPrice) {
     let totalQuantity = 0;
     const productList = orderItems.map(({ option, quantity, item }) => {
@@ -52,6 +55,7 @@ function setOrderList(date, id, addressee, orderItems, totalPrice) {
     orderId += 1;
 }
 
+// 배송상태별 주문 목록 요청
 async function insertOrderList() {
     // const url = './orderlistdata.json';    // 임시 데이터
     const url = '/api/v1/orders/shipping/1/20';
@@ -89,6 +93,7 @@ async function insertOrderList() {
     }
 }
 
+// 주문의 배송상태를 배송완료로 변경 요청
 async function changeStatus(idList) {
     const url = `/api/v1/orders/update/status`;
 
@@ -116,6 +121,7 @@ async function changeStatus(idList) {
     }
 }
 
+// 선택한 주문만 배송상태 변경
 function orderDelivered() {
     const checkList = document.querySelectorAll("#check-item");
 
@@ -132,6 +138,7 @@ function orderDelivered() {
         return;
     }
 
+    // eslint-disable-next-line no-alert
     if (window.confirm('선택한 주문의 배송상태를 "배송완료"로 변경하시겠습니까?')) {
         while (shipptingListEl.firstChild) {
             shipptingListEl.removeChild(shipptingListEl.firstChild);
